bcdf7d0b6b11bd7892707b02c1d21b63bf3b9258
Rishabh Harshadray roy <riro002@ess.ais.ac.nz>
2023-03-06 15:30:44 +1300

    Add project files.

---
+++ IdentityDemo/Areas/Data/AppUsers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDemo.Areas.Data
+{
+    public class AppUsers : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? MobileNo { get; set; }
+        public string? Address { get; set; }
+        public object FullName { get; internal set; }
+    }
+}
---
+++ IdentityDemo/Data/Migrations/20230226054333_MobileNo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityDemo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MobileNo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNo",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNo",
+                table: "AspNetUsers");
+        }
+    }
+}
